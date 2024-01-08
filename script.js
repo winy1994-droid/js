319545729da4e442ad85412612f65f83a067b2f9
hasha <137384828+winy1994-droid@users.noreply.github.com>
2024-01-08 10:57:45 +0330

    another example

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
- 
- function ball1(ball2){
-   setTimeout(()=>{
-    console.log(" the ball one reached the end of hill");
-    ball2(ball3)
-   },2000)
- }
-
- function ball2(ball3){
-  setTimeout(()=>{
-   console.log(" the ball  two reached the end of hill");
-   ball3()
-  },1000)
+function person1(person2) {
+  setTimeout(() => {
+    console.log("the person1 reached the end of line");
+    person2(person3);
+  }, 2000);
 }
 
-function ball3(){
-  setTimeout(()=>{
-   console.log(" the ball three reached the end of hill");
-  },3000)
+function person2(person3) {
+  setTimeout(() => {
+    console.log("the person2 reached the end of line");
+    person3();
+  }, 1000);
 }
 
-ball1(ball2)
- 
+function person3() {
+  setTimeout(() => {
+    console.log("the person3 reached the end of line");
+  }, 4000);
+}
 
 
+person1(person2)
