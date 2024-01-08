a041c12ced8da7e10851e3fae6b08f538ec69472
hasha <137384828+winy1994-droid@users.noreply.github.com>
2024-01-08 15:35:37 +0330

    promise

--- script.js
+++ script.js
@@ -1,22 +1,87 @@
-function person1(person2) {
-  setTimeout(() => {
-    console.log("the person1 reached the end of line");
-    person2(person3);
-  }, 2000);
+function ball1() {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("the ball1 reached the end of the hill");
+    } else {
+      reject("the ball1 failed");
+    }
+  });
 }
 
-function person2(person3) {
-  setTimeout(() => {
-    console.log("the person2 reached the end of line");
-    person3();
-  }, 1000);
+function ball2() {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("the ball2 reached the end of the hill");
+    } else {
+      reject("the ball2 failed");
+    }
+  });
+}
+
+function ball3() {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("the ball3 reached the end of the hill");
+    } else {
+      reject("the ball3 failed");
+    }
+  });
+}
+
+ball1()
+  .then((msg) => {
+    console.log(msg);
+    return ball2();
+  })
+  .then((msg) => {
+    console.log(msg);
+    return ball3();
+  })
+  .then((msg) => {
+    console.log(msg);
+  })
+  .catch((e) => console.log(e));
+
+function person1() {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("the person one won");
+    } else {
+      reject("the person one failed");
+    }
+  });
+}
+
+function person2() {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("the person two won");
+    } else {
+      reject("the person2 failed");
+    }
+  });
 }
 
 function person3() {
-  setTimeout(() => {
-    console.log("the person3 reached the end of line");
-  }, 4000);
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("the person 3 won");
+    } else {
+      reject("the person3 failed");
+    }
+  });
 }
 
-
-person1(person2)
+person1()
+  .then((msg) => {
+    console.log(msg);
+    return person2();
+  })
+  .then((msg) => {
+    console.log(msg);
+    return person3();
+  })
+  .then((msg) => {
+    console.log(msg);
+  })
+  .catch((e) => console.log(e));
