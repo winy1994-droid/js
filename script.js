6fde9e7a61302f87619a51d52718017821cd8a20
hasha <137384828+winy1994-droid@users.noreply.github.com>
2024-01-08 17:57:33 +0330

    async await

--- script.js
+++ script.js
@@ -1,87 +1,35 @@
 function ball1() {
-  return new Promise((resolve, reject) => {
-    if (true) {
-      resolve("the ball1 reached the end of the hill");
-    } else {
-      reject("the ball1 failed");
-    }
-  });
+  if (true) {
+    return "the ball1 reached the end of the hill";
+  } else {
+    throw new Error("the ball1 failed");
+  }
 }
 
 function ball2() {
-  return new Promise((resolve, reject) => {
-    if (true) {
-      resolve("the ball2 reached the end of the hill");
-    } else {
-      reject("the ball2 failed");
-    }
-  });
+  if (true) {
+    return "the ball2 reached the end of the hill";
+  } else {
+    throw new Error("the ball2 failed");
+  }
 }
 
 function ball3() {
-  return new Promise((resolve, reject) => {
-    if (true) {
-      resolve("the ball3 reached the end of the hill");
-    } else {
-      reject("the ball3 failed");
-    }
-  });
+  if (true) {
+    return "the ball3 reached the end of the hill";
+  } else {
+    throw new Error("the ball3 failed");
+  }
 }
 
-ball1()
-  .then((msg) => {
-    console.log(msg);
-    return ball2();
-  })
-  .then((msg) => {
-    console.log(msg);
-    return ball3();
-  })
-  .then((msg) => {
-    console.log(msg);
-  })
-  .catch((e) => console.log(e));
-
-function person1() {
-  return new Promise((resolve, reject) => {
-    if (true) {
-      resolve("the person one won");
-    } else {
-      reject("the person one failed");
-    }
-  });
+async function execute() {
+try {
+  console.log(await ball1());
+  console.log(await ball2());
+  console.log(await ball3());
+} catch (error) {
+  console.log(error);
+}
 }
 
-function person2() {
-  return new Promise((resolve, reject) => {
-    if (true) {
-      resolve("the person two won");
-    } else {
-      reject("the person2 failed");
-    }
-  });
-}
-
-function person3() {
-  return new Promise((resolve, reject) => {
-    if (true) {
-      resolve("the person 3 won");
-    } else {
-      reject("the person3 failed");
-    }
-  });
-}
-
-person1()
-  .then((msg) => {
-    console.log(msg);
-    return person2();
-  })
-  .then((msg) => {
-    console.log(msg);
-    return person3();
-  })
-  .then((msg) => {
-    console.log(msg);
-  })
-  .catch((e) => console.log(e));
+execute();
